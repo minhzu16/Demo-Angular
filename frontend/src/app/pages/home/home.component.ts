7f0c8eaaec1aa9564363758d010dced9dc0503c3
minhvu16 <minhvu160105@gmail.com>
2025-09-08 13:52:25 +0700

    chore: add frontend as regular folder

---
+++ frontend/src/app/pages/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  private auth = inject(AuthService);
+
+  logout() {
+    this.auth.logout();
+  }
+}
